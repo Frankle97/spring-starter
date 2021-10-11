606a2770e215399add22bc96b9cd7052ac055d7e
Frankle97 <pairpapa@gmail.com>
2021-10-11 15:24:30 +0900

    빈 스코프

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -36,7 +36,7 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean1.logic();
-        assertThat(count2).isEqualTo(1);
+        assertThat(count2).isEqualTo(2);
     }
 
     @Scope("singleton")
